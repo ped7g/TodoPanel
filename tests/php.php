aefef3b6a577b69f9780c793fa63ad38bf60bdc0
Peter Ped Helcmanovsky <kukadlo@pobox.sk>
2010-05-19 13:16:26 +0200

    some more tricky PHP comments (to parse correctly)

--- tests/php.php
+++ tests/php.php
@@ -7,7 +7,11 @@
 
 <?php
 
-// TODO s falesnym /* block commentem */
+// TODO s falesnym /* block commentem
+//TODO a bez uzavreni falesneho bloku
+/* blokovy comment
+ * // s falesnym radkovym TODO nic */
+/* /* TODO blokovy nested comment /* (bad practice) */
 $x = '/* TODO v stringu by se asi najit nemelo */;
 
 /* TODO blokove */ unset( $x ); //TODO radkove na stejnem radku jako blokove
